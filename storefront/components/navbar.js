6d775b2c10d60d80a25012104d25ada47e512322
janfabian <mail@janfabian.cz>
2022-06-11 18:22:04 +0200

    links const

--- storefront/components/navbar.js
+++ storefront/components/navbar.js
@@ -4,13 +4,21 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "next/router";
 
+const LINKS = {
+  QUESTS: "/quests",
+  STORE: "/store",
+  FAQ: "/faq",
+};
+
 export default function Navbar() {
   const router = useRouter();
 
   const [value, setValue] = useState("/quests");
 
   useEffect(() => {
-    setValue(router.asPath);
+    if (Object.values(LINKS).includes(router.asPath)) {
+      setValue(router.asPath);
+    }
   }, [router.asPath]);
 
   const { t } = useTranslation("Navbar");
@@ -27,9 +35,9 @@ export default function Navbar() {
         onChange={handleChange}
         value={value}
       >
-        <Tab label={t("Quests")} value="/quests" />
-        <Tab label={t("Store")} value="/store" />
-        <Tab label={t("FAQ")} value="/faq" />
+        <Tab label={t("Quests")} value={LINKS.QUESTS} />
+        <Tab label={t("Store")} value={LINKS.STORE} />
+        <Tab label={t("FAQ")} value={LINKS.FAQ} />
       </Tabs>
     </Box>
   );
