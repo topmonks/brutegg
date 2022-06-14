7b2bd184132bb4b37450334f3f4c2ecb44f8ca19
janfabian <mail@janfabian.cz>
2022-06-14 15:02:14 +0200

    navbar

--- storefront/components/navbar.js
+++ storefront/components/navbar.js
@@ -1,4 +1,5 @@
-import { Tab, Tabs } from "@mui/material";
+import { Tab, Tabs, alpha, Grid } from "@mui/material";
+import Image from "next/image";
 import { Box } from "@mui/system";
 import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -6,7 +7,33 @@ import { useRouter } from "next/router";
 import { withLocale } from "../libs/router";
 import { useRecoilValue } from "recoil";
 import { eventTargetState, NAVBAR_CHANGE } from "../state/event-target";
+import { keyframes } from "@emotion/react";
 import window from "../libs/window";
+import styled from "@emotion/styled";
+
+const backgroundAnimation = keyframes`
+  from {
+    background-size:500% 200%;
+  }
+  to {
+    background-size:100% 100%;
+  }
+`;
+
+const CustomTab = styled((props) => <Tab {...props} />)(({ theme }) => ({
+  "&:hover": {},
+  "&.Mui-selected": {
+    background: `radial-gradient(ellipse at 50% 300%, ${
+      theme.palette.primary.main
+    }, ${alpha(theme.palette.primary.main, 0)} 80%) center no-repeat`,
+    animation: `${backgroundAnimation} 1s ease-out`,
+    color: "white",
+  },
+  fontSize: "20px",
+  fontWeight: "bold",
+  color: "white",
+  textShadow: `0px 0px 12px ${theme.palette.primary.main}`,
+}));
 
 export const LINKS = {
   QUESTS: "/quests",
@@ -49,25 +76,46 @@ export default function Navbar() {
 
   return (
     <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-      <Tabs
-        aria-label="basic tabs example"
-        indicatorColor="secondary"
-        onChange={handleChange}
-        value={value}
+      <Grid
+        alignItems="baseline"
+        container
+        direction="row"
+        justifyContent="space-between"
       >
-        {[
-          [t("Quests"), LINKS.QUESTS],
-          [t("Store"), LINKS.STORE],
-          [t("FAQ"), LINKS.FAQ],
-        ].map(([label, value]) => (
-          <Tab
-            key={value}
-            label={label}
-            onClick={(e) => handleChange(e, value)}
+        <Grid item sm={5} xs={12}>
+          <Tabs
+            allowScrollButtonsMobile
+            aria-label="basic tabs example"
+            indicatorColor="secondary"
+            onChange={handleChange}
+            scrollButtons="auto"
             value={value}
+            variant="scrollable"
+          >
+            {[
+              [t("Quests"), LINKS.QUESTS],
+              [t("Store"), LINKS.STORE],
+              [t("FAQ"), LINKS.FAQ],
+            ].map(([label, value]) => (
+              <CustomTab
+                key={value}
+                label={label}
+                onClick={(e) => handleChange(e, value)}
+                value={value}
+              />
+            ))}
+          </Tabs>
+        </Grid>
+        <Grid item sm={2} sx={{ display: { xs: "none", sm: "block" } }}>
+          <Image
+            alt="Picture of the author"
+            height={80}
+            src="/brute-logo.svg"
+            width={200}
           />
-        ))}
-      </Tabs>
+        </Grid>
+        <Grid item sm={5} sx={{ display: { xs: "none", sm: "block" } }}></Grid>
+      </Grid>
     </Box>
   );
 }
