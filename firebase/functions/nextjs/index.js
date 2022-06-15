d1956acfe84ad0da25ba0166f1850f44426f1cc9
janfabian <mail@janfabian.cz>
2022-06-15 10:48:36 +0200

    2GiB lambda

--- firebase/functions/nextjs/index.js
+++ firebase/functions/nextjs/index.js
@@ -15,7 +15,7 @@ export const server = onRequest(
     cors: ["brute-gg.web.app"],
     // firebase hosting only supports us-central1
     region: "us-central1",
-    memory: "1GiB",
+    memory: "2GiB",
     maxInstances: 10,
   },
   (request, response) => {
