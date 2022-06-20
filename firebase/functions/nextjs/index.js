8cecb92ffdb4b45be6c08c376f23424de4e49e37
janfabian <mail@janfabian.cz>
2022-06-20 14:30:57 +0200

    edit cache for routes

--- firebase/functions/nextjs/index.js
+++ firebase/functions/nextjs/index.js
@@ -10,26 +10,41 @@ const app = next({
 });
 const handle = app.getRequestHandler();
 
+const CACHE_EXCLUDE_PATH_REGEX = [
+  /\/store(\.json)?$/,
+  /\/store\/item\/.+\/.+$/,
+  /\/quests(\.json)?$/,
+  /\/quests\/.+\/.+$/,
+  /\/faq(\.json)?$/,
+];
+
 export const server = onRequest(
   {
     cors: ["brute-gg.web.app"],
     // firebase hosting only supports us-central1
     region: "us-central1",
     memory: "2GiB",
-    maxInstances: 10,
+    maxInstances: 100,
   },
   (request, response) => {
     logger.info("File: " + request.originalUrl, {
       structuredData: true,
     });
 
-    response.set("Cache-Control", "public, max-age=300, s-maxage=86400"); //max-age=5minutes, s-maxage=1 day
-
     if ([".js", ".css"].find((ext) => request.originalUrl.endsWith(ext))) {
       response.set(
         "Cache-Control",
         "public, max-age=31536000, s-maxage=31536000"
       ); //max-age=1year, s-maxage=1year
+    }
+
+    if (CACHE_EXCLUDE_PATH_REGEX.find((r) => r.test(request.path))) {
+      logger.info("Exclude: " + request.path, {
+        structuredData: true,
+      });
+    } else {
+      // default cache
+      response.set("Cache-Control", "public, max-age=300, s-maxage=86400"); //max-age=5minutes, s-maxage=1 day
     }
 
     return app.prepare().then(() => handle(request, response));
