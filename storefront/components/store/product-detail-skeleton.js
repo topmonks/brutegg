6b1062e72f15daf8de464139f07a955c1603de9c
janfabian <mail@janfabian.cz>
2022-07-13 12:47:48 +0200

    detail skeleton

--- storefront/components/store/product-detail-skeleton.js
+++ storefront/components/store/product-detail-skeleton.js
@@ -1,10 +1,11 @@
 import { Box } from "@mui/system";
-import { Typography, Skeleton } from "@mui/material";
+import { Skeleton, useMediaQuery } from "@mui/material";
 import { Fragment } from "react";
 import useDisplayAfterDelay from "../../hooks/use-display-after-delay";
 
 export function ProductDetailSkeleton() {
   const display = useDisplayAfterDelay(200);
+  const isXs = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
   if (!display) {
     return <Fragment></Fragment>;
@@ -12,13 +13,15 @@ export function ProductDetailSkeleton() {
 
   return (
     <Fragment>
-      <Skeleton animation="wave" height={"20%"} variant="rectangular" />
+      <Box sx={{ height: "20%", px: 3, py: 2 }}>
+        <Skeleton animation="wave" height="100%" variant="rectangular" />
+      </Box>
 
       <Box
         sx={{
           display: "flex",
-          my: 1,
-          height: "5%",
+          px: 3,
+          height: "40px",
           justifyContent: "space-between",
         }}
       >
@@ -26,23 +29,22 @@ export function ProductDetailSkeleton() {
           animation="wave"
           height={"100%"}
           variant="rectangular"
-          width={200}
+          width={isXs ? 120 : 200}
         />
         <Skeleton
           animation="wave"
           height={"100%"}
           variant="rectangular"
-          width={150}
+          width={isXs ? 120 : 250}
         />
       </Box>
 
-      <Skeleton animation="wave" height={"50%"} variant="rectangular" />
-      <Skeleton
-        animation="wave"
-        height={"20%"}
-        sx={{ mt: 1 }}
-        variant="rectangular"
-      />
+      <Box sx={{ height: "50%", px: 3, py: 2 }}>
+        <Skeleton animation="wave" height="100%" variant="rectangular" />
+      </Box>
+      <Box sx={{ height: "calc(30% - 40px)", px: 1, pb: 1 }}>
+        <Skeleton animation="wave" height="100%" variant="rectangular" />
+      </Box>
     </Fragment>
   );
 }
