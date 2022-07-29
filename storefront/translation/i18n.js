ae4147f5586f7938f5a597baf4943d0ab8c83f9e
janfabian <mail@janfabian.cz>
2022-07-29 12:06:31 +0200

    en i18n added

--- storefront/translation/i18n.js
+++ storefront/translation/i18n.js
@@ -1,9 +1,11 @@
 import { initReactI18next } from "react-i18next";
 import cs from "./cs.json";
+import en from "./en.json";
 import i18n from "i18next";
 
 const resources = {
   cs,
+  en,
 };
 
 i18n.use(initReactI18next).init({
