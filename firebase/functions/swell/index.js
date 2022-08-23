8f0c38e16a88568971b618d47f18f56d50d9f17b
janfabian <mail@janfabian.cz>
2022-08-23 09:55:34 +0200

    recalculate spent reset users

--- firebase/functions/swell/index.js
+++ firebase/functions/swell/index.js
@@ -8,6 +8,20 @@ export const swellNodeClient = swell.createClient(
   process.env.SWELL_SECRET_KEY
 );
 
+async function* iterate(query) {
+  let page = 1;
+  let hasNextPage = true;
+
+  while (hasNextPage && page < 100 /* safety break */) {
+    let result = await query(page);
+
+    yield result;
+
+    page += 1;
+    hasNextPage = Boolean(result.pages?.[page]);
+  }
+}
+
 export const recalculatespent = onMessagePublished(
   {
     topic: "projects/brute-gg/topics/recalculate-brute-spent",
@@ -19,21 +33,17 @@ export const recalculatespent = onMessagePublished(
   },
   async () => {
     let LIMIT = 1000;
-    let page = 1;
-    let hasNextPage = true;
 
     const usersSpent = {};
 
-    while (hasNextPage && page < 100 /* safety break */) {
-      let result = await swellNodeClient.get("/orders", {
+    const ordersQuery = (page) =>
+      swellNodeClient.get("/orders", {
         limit: LIMIT,
         page: page,
         expand: ["items.product"],
       });
 
-      page += 1;
-      hasNextPage = Boolean(result.pages?.[page]);
-
+    for await (const result of iterate(ordersQuery)) {
       for (const order of result.results) {
         /**
          * @type {import("bn.js")}
@@ -49,6 +59,36 @@ export const recalculatespent = onMessagePublished(
         usersSpent[userId] = usersSpent[userId]
           ? usersSpent[userId].add(total)
           : total;
+      }
+    }
+
+    // clear current brute spent, before new amount is assigned
+    // in cases where orders of some users get deleted
+    const usersQuery = (page) =>
+      swellNodeClient.get("/accounts", {
+        limit: LIMIT,
+        page: page,
+        where: {
+          spent_brute: {
+            $ne: null,
+          },
+        },
+      });
+
+    const userIdsWithSpent = Object.keys(usersSpent);
+
+    for await (const result of iterate(usersQuery)) {
+      for (const user of result.results) {
+        // user with some spent will be updated anyway
+        if (userIdsWithSpent.includes(user.id)) {
+          continue;
+        }
+
+        await swellNodeClient.put("/accounts/{id}", {
+          id: user.id,
+          order_value: 0,
+          spent_brute: null,
+        });
       }
     }
 
